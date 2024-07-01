915be1d280680d3a15a03c20d81d75090e17aa81
Ramon Mendes <141190770+MendesRamon@users.noreply.github.com>
2024-06-30 22:21:59 -0300

    Update 1_🏠_home.py

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -19,9 +19,11 @@ st.markdown("# FIFA23 OFFICIAL DATASET! ⚽️")
 st.sidebar.markdown("Desenvolvido por [Ramon Mendes](https://github.com/MendesRamon) no curso da Asimov Academy")
 
 
-btn = st.link_button("Acesse os dados no Kaggle", 
-                           "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data"
-                           ) #botão para acessar os dados do site
+btn = st.link_button(
+    "Acesse os dados no Kaggle", 
+    "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data"
+    ) #botão para acessar os dados do site
+
 st.markdown(
     """
     O conjunto de dados
